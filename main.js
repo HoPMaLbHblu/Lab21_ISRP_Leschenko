82a7fb6fefad8ac26a002d7235d580ec637e2c89
Dan <vfvf5389127@gmail.com>
2026-03-30 01:40:36 +0300

    Implemented start/stop functionality

---
+++ main.js
@@ -0,0 +1,41 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let timer = null;
+
+const time = document.getElementById("time");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+function updateTime() {
+    seconds++;
+
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let h = hours < 10 ? "0" + hours : hours;
+
+    time.textContent = `${h}:${m}:${s}`;
+}
+
+startBtn.onclick = function () {
+    if (timer !== null) return;
+    timer = setInterval(updateTime, 1000);
+};
+
+stopBtn.onclick = function () {
+    clearInterval(timer);
+    timer = null;
+};
+
